067a3cefa28280c72af1b90be4a28042c7270d37
Fatih BAYTAR <matanist@hotmail.com>
2023-04-12 17:14:09 +0100

    sadfasdf

---
+++ Data/Store.cs
@@ -0,0 +1,16 @@
+namespace loginuser.Data;
+
+public static class Store
+{
+    public static List<User> Users = new List<User>{
+        new User{Id=1,Username="user1",FirstName="Fatih",LastName="Baytar"},
+        new User{Id=2,Username="user2",FirstName="Fatih",LastName="Baytar"},
+        new User{Id=3,Username="user3",FirstName="Fatih",LastName="Baytar"},
+        new User{Id=4,Username="user4",FirstName="Fatih",LastName="Baytar"},
+        new User{Id=5,Username="user5",FirstName="Fatih",LastName="Baytar"},
+        new User{Id=6,Username="user6",FirstName="Fatih",LastName="Baytar"},
+        new User{Id=7,Username="user7",FirstName="Fatih",LastName="Baytar"},
+        new User{Id=8,Username="user8",FirstName="Fatih",LastName="Baytar"},
+    };
+
+}
